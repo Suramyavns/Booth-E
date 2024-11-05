299a63e9d2b776b19e1cb44c17af1cfc691431b6
suramyavns <sdidwania645@gmail.com>
2024-11-06 01:21:22 +0530

    style fix

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "./form";
 export default function Login(){
     return(
         <div className="w-full flex justify-center p-4 items-center gap-4">
-            <span className="flex w-full h-full justify-center items-center">
+            <span className="hidden lg:flex w-full h-full justify-center items-center">
                 <img
                 style={{
                     backgroundColor:'#333533'
--- src/app/(auth)/register/layout.tsx
+++ src/app/(auth)/register/layout.tsx
@@ -2,7 +2,7 @@ export default function RegisterLayout({children}:{
     children:React.ReactNode
 }){
     return(
-        <div className="login-layout lg:w-4/5 h-full flex flex-col gap-8 p-10 justify-center items-center">
+        <div className="login-layout lg:w-4/5 h-full flex flex-col gap-4 p-10 justify-center items-center">
             <p className="text-center lg:self-start text-3xl md:text-4xl lg:text-5xl font-bold">
                 Register here!
             </p>
